7c320b6d4abafb5f2e8665daf14498a27c40f801
@willhead <60652390+will-head@users.noreply.github.com>
2020-04-09 12:40:56 +0100

    Add fizzbuss and number tests, update code to pass tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,13 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(10)).to eq 'buzz'
   end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  
+  it 'returns number when passed anything esle' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+
 end
